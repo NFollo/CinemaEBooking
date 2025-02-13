33688d11e3fe822ce21652559861f6f1c665a346
husky3802 <nfollo7@gmail.com>
2025-02-13 14:00:19 -0500

    added some login stuff
    currently bugged, text not displaying

---
+++ cinemaebooking/src/components/LoginForm.js
@@ -0,0 +1,18 @@
+import "./LoginForm.css";
+import { Link } from 'react-router-dom';
+
+function LoginForm() {
+    return (
+      <div className="LoginForm">
+        hi        
+      </div>
+    );
+}
+  
+export default LoginForm;
+
+/*
+<Link to={'/'}>
+          <button className="loginhome">Home</button>
+        </Link>  
+*/
--- cinemaebooking/src/components/LoginPage.js
+++ cinemaebooking/src/components/LoginPage.js
@@ -1,12 +1,17 @@
 import "./LoginPage.css";
+import { Link } from 'react-router-dom';
+import LoginSignupCard from './LoginSignupCard';
+import LoginForm from "./LoginForm";
 
 function LoginPage() {
-
   return (
     <div className="LoginPage">
-      hi
+      <Link to={'/'}>
+        <button className="loginhome">Home</button>
+      </Link>  
+      <LoginSignupCard text={<LoginForm />} /> 
     </div>
   );
 }
-
+/*this is bugged, it is not passing LoginForm properly*/
 export default LoginPage;
