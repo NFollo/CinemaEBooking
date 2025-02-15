ff63c70e19535332ecd436138c144f8c96a2a046
Regina Dong <rd88449@uga.edu>
2025-02-15 15:58:42 -0500

    profile icon

--- cinemaebooking/src/components/NavBar.js
+++ cinemaebooking/src/components/NavBar.js
@@ -1,8 +1,25 @@
 import "./NavBar.css";
 import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import createAcc from "../img/create-acc.png";
+import login from "../img/login.png";
+// import user from "../img/user.png"; 
+// import edit from "../img/edit.png";
+// import logout from "../img/log-out.png";
+
+function DropdownItem({ img, text }) {
+  return (
+    <li className="dropdownItem">
+      <img src={img} alt="icon" className="dropdownIcon" />
+      <span>{text}</span>
+    </li>
+  );
+}
 
 function NavBar() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
     <nav className="navbar">
@@ -18,12 +35,27 @@ function NavBar() {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className="searchButton" onClick={() => alert(`Searching for: ${searchTerm}`)}>🔍</button>
+        <button className="searchButton">
+          <FaSearch />
+        </button>
       </div>
 
       <div className="rightPart">
-        <button className="signUp">Sign Up</button>
-        <button className="login">Login</button>
+        <div className="menu-container">
+          <div
+            className="profile"
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <CgProfile size={28} className="profileIcon" />
+          </div>
+          <ul className={`dropdown-menu ${openMenu ? "active" : ""}`}>
+            <DropdownItem img={createAcc} text="Create Account" />
+            <DropdownItem img={login} text="Login" />
+            {/* <DropdownItem img={user} text="My Profile" />
+            <DropdownItem img={edit} text="Edit Profile" />
+            <DropdownItem img={logout} text="Logout" /> */}
+          </ul>
+        </div>
       </div>
     </nav>
   );
