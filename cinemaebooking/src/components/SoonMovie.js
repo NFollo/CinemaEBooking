1ced3165141528e158807b987a299fc24c648464
Regina Dong <rd88449@uga.edu>
2025-02-16 01:32:01 -0500

    buy tickets button

--- cinemaebooking/src/components/SoonMovie.js
+++ cinemaebooking/src/components/SoonMovie.js
@@ -1,12 +1,26 @@
-import './SoonMovie.css';
+import { useState } from "react";
+import "./SoonMovie.css";
 
 function SoonMovie() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <div className="SoonMovie">
-        <img src="https://connect.gtcmovies.com/CDN/Image/Entity/FilmPosterGraphic/HO00004607" alt="poster" />
-        <div className="SoonMovieTitle">
-            Dog Man
-        </div>
+      <div 
+        className={`SoonMovieImg ${isClicked ? "clicked" : ""}`} 
+        onClick={() => setIsClicked(!isClicked)} 
+      >
+        <img 
+          src="https://connect.gtcmovies.com/CDN/Image/Entity/FilmPosterGraphic/HO00004607" 
+          alt="poster"
+        />
+        {isClicked && (
+          <button className="overlayText">Buy Tickets</button>
+          )}
+      </div>
+      <div className="SoonMovieTitle">
+        Dog Man
+      </div>
     </div>
   );
 }
