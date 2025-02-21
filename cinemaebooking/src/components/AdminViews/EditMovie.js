e900baf5a8647809747a1f6af31dd5374ae8f3fc
husky3802 <nfollo7@gmail.com>
2025-02-20 19:04:11 -0500

    added time slots for admin edit of movie

--- cinemaebooking/src/components/AdminViews/EditMovie.js
+++ cinemaebooking/src/components/AdminViews/EditMovie.js
@@ -1,11 +1,14 @@
 import "./AdminEdit.css";
 import { useNavigate } from 'react-router-dom';
+//import { useState } from "react";
 
 function EditMovie() {
 
+  //const [selectedShowtime, setSelectedShowtime] = useState("");
+
   const navigate = useNavigate();
-  const navProfiles = () => {
-    navigate("/manageusers");
+  const navMovies = () => {
+    navigate("/managemovies");
   };
 
   return (
@@ -34,12 +37,30 @@ function EditMovie() {
             <div className="AdminFormSection">
                 View Date 1:
             <input type="date"></input>
-            </div>           
+            </div>  
+
+            <div>Times:</div>
+            <div className="AdminShowtimeButtons">
+              {["10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 AM", "3:00 AM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM", "10:00 PM"].map((time) => (
+                <label>
+                <input 
+                  type="checkbox"
+                  key={time}
+                  className={"twelve"}
+                  onClick={(e) => {
+                    
+                  }}
+                >
+                </input>
+                <span>{time}</span>
+                </label>
+              ))}
+
+            </div>     
             
-            <div className="AdminFormSection">
-                Other:
-                <input type="text"></input>
-            </div>              
+             
+
+            <input type="submit" value="Save Changes" onClick={navMovies} className="AdminEditSave"></input>            
             
         </form>
 
