1b49427062f094ff6ed679a4b7df429b5cf898c6
cccooper6 <ccc59647@uga.edu>
2025-02-26 21:07:06 -0500

    Updated EditProfile w/ card and home address

--- cinemaebooking/src/components/LoggedInView/EditProfile.js
+++ cinemaebooking/src/components/LoggedInView/EditProfile.js
@@ -25,58 +25,93 @@ function EditProfile() {
             <div>Phone Number:</div>
             <input type="text"></input>
 
-            <div className="EditProfileSubtitle">Card 1 Information</div>
-            <div>Card Type:</div>
-            <select name="cardtype">
-              <option value="none"></option>
-              <option value="credit">Credit</option>
-              <option value="debit">Debit</option>
-            </select>
+            <div className="cardContainer">
+                <div className="cardInformationContainer">
+                    <div className="EditProfileSubtitle">Card 1 Information</div>
+                    <div>Card Type:</div>
+                    <select name="cardtype">
+                    <option value="none"></option>
+                    <option value="credit">Credit</option>
+                    <option value="debit">Debit</option>
+                    </select>
 
-            
-            <div className="SignupFormSection">
-            
+                    
+                    <div className="SignupFormSection">
+                    
+                    </div>
+                    <div className="SignupFormSection">
+                        Name (as appears on card):
+                        <input type="text"></input>
+                    </div>  
+                    <div className="SignupFormSection">
+                        Card Number:
+                    <input type="number"></input>
+                    </div>
+                    <div className="SignupFormSection">
+                        Expiration Date:
+                    <input type="date"></input>
+                    </div>
+                    <div className="SignupFormSection">
+                        CVC:
+                    <input type="number"></input>
+                    </div>
+                </div>
+                <div className="cardAddressContainer">
+                    <div className="EditProfileSubtitle">Card 1 Billing Address Information</div>
+
+                    <div className="SignupFormSection">
+                        Country:
+                    <input type="text"></input>
+                    </div>
+                    <div className="SignupFormSection">
+                        State:
+                    <input type="text"></input>
+                    </div>
+                    <div className="SignupFormSection">
+                        City:
+                    <input type="text"></input>
+                    </div>
+                    <div className="SignupFormSection">
+                        Address:
+                    <input type="text"></input>
+                    </div>
+                    <div className="SignupFormSection">
+                        Zip Code:
+                    <input type="number"></input>
+                    </div>
+                </div>
+
+                <button className="deleteCardButton">Delete Card 1</button>
             </div>
-            <div className="SignupFormSection">
-                Name (as appears on card):
-                <input type="text"></input>
-            </div>  
-            <div className="SignupFormSection">
-                Card Number:
-            <input type="number"></input>
-            </div>
-            <div className="SignupFormSection">
-                Expiration Date:
-            <input type="date"></input>
-            </div>
-            <div className="SignupFormSection">
-                CVC:
-            <input type="number"></input>
+            <div></div>
+            <div><button className="addNewCardButton">Add New Card</button></div>
+
+            <div className="addressInformationContainer">
+
             </div>
 
-            <div className="EditProfileSubtitle">Card 1 Billing Address Information</div>
+        <div className="SignupFormSection">
+            Country:
+          <input type="text"></input>
+          </div>
+          <div className="SignupFormSection">
+            State:
+          <input type="text"></input>
+          </div>
+          <div className="SignupFormSection">
+            City:
+          <input type="text"></input>
+          </div>
+          <div className="SignupFormSection">
+            Address:
+          <input type="text"></input>
+          </div>
+          <div className="SignupFormSection">
+            Zip Code:
+          <input type="number"></input>
+          </div>
 
-            <div className="SignupFormSection">
-                Country:
-            <input type="text"></input>
-            </div>
-            <div className="SignupFormSection">
-                State:
-            <input type="text"></input>
-            </div>
-            <div className="SignupFormSection">
-                City:
-            <input type="text"></input>
-            </div>
-            <div className="SignupFormSection">
-                Address:
-            <input type="text"></input>
-            </div>
-            <div className="SignupFormSection">
-                Zip Code:
-            <input type="number"></input>
-            </div>
-            <input type="submit" value="Save Changes" onClick={navViewProfile} className="EditProfileSave"></input>
+          <input type="submit" value="Save Changes" onClick={navViewProfile} className="EditProfileSave"></input>
             
         </form>
       </div>
