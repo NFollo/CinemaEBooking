f61700019d1b237db07c81ea5aae979d0cf67b3e
husky3802 <nfollo7@gmail.com>
2025-03-23 21:04:12 -0400

    Update View Profile (pulls from DB)
    general info pulls from database, none of the other info does as of now

--- cinemaebooking/src/components/LoggedInView/ViewProfile.js
+++ cinemaebooking/src/components/LoggedInView/ViewProfile.js
@@ -1,16 +1,18 @@
 import "./ViewProfile.css";
 import { useNavigate } from 'react-router-dom';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
+import axios from "axios";
 
 function ViewProfile() {
 
     // do not use this data as a reference please change the names of all the variables as necessary
-    const baseUser = {
-      username: "Spongebob Squarepants",
-      phone: "111-222-3333",
+    var user = {
+      first_name: "",
+      last_name: "",
+      phone: "",
       email: Cookies.get("email"),
-      password: "myPassword454",
+      password: "",
 
       cardtype: "Credit",
       cardnum: 1111222233334567,
@@ -26,7 +28,25 @@ function ViewProfile() {
       zipcode: 12345
     };
 
-    const [user, setUser] = useState(baseUser);
+    const [first_name, setFirst_name] = useState("");
+    const [last_name, setLast_name] = useState("");
+    const [phone_number, setPhone_number] = useState("");
+
+    // Include all requests and set values here
+    useEffect(() => {
+    const getAllData = async() => {
+      try {
+        var response = await axios.get('http://localhost:5000/users/njf22289@uga.edu');
+        //console.log(response.data);
+        setFirst_name(response.data.first_name);
+        setLast_name(response.data.last_name);
+        setPhone_number(response.data.phone_number);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+      };
+      getAllData(); // I feel like this line should be inside the curly brackets above, but it works for now
+    }, []);
 
     const navigate = useNavigate();
     const navEditProfile = () => {
@@ -40,9 +60,10 @@ function ViewProfile() {
         </div>
         <div className="ViewProfileInfo">
             <div className="ViewProfileSubtitle">General Information</div>
-            <div>Email: <span>{user.email}</span></div>
-            <div>Username: <span>{user.username}</span></div>
-            <div>Phone Number: <span>{user.phone}</span></div>            
+            <div>Email: <span>{Cookies.get("email")}</span></div>
+            <div>First Name: <span>{first_name}</span></div>
+            <div>Last Name: <span>{last_name}</span></div>
+            <div>Phone Number: <span>{phone_number}</span></div>            
             <div>Password: <span>********</span></div>
 
             <div className="ViewProfileSubtitle">Home Address</div>
